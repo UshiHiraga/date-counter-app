358d493749bae4c464ed9f8a65ed357d2250f941
Ushi Hiraga <ushi.hiraga@gmail.com>
2022-08-27 01:37:06 -0500

    Improved new feature in the activity

--- app/src/main/java/com/ushihiraga/datecounter/ConfigurationActivity.kt
+++ app/src/main/java/com/ushihiraga/datecounter/ConfigurationActivity.kt
@@ -1,11 +1,47 @@
 package com.ushihiraga.datecounter
 
+import android.appwidget.AppWidgetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.time.LocalDate
 
 class ConfigurationActivity : AppCompatActivity() {
+    private var widgetId: Int? = intent.extras?.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
+    private var eventDate: LocalDate = LocalDate.now()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuration)
+        setResult(RESULT_CANCELED)
+
+
     }
+
+    fun saveWidget(view: View) {
+        val eventTitle: String = findViewById<EditText>(R.id.eventTitleInput).text.toString()
+        val eventDescription: String = findViewById<EditText>(R.id.eventDescriptionInput).text.toString()
+        val widgetData = mapOf("title" to eventTitle, "description" to eventDescription, "time" to eventDate.toString())
+
+        val b: Button = findViewById(view.id)
+        b.text = widgetData.toString()
+
+    }
+
+    private fun onDateSelected(selectedDate: LocalDate) {
+        val selectDateButton: Button = findViewById(R.id.eventDateInput)
+        Toast.makeText(this, selectedDate.toString(), Toast.LENGTH_LONG).show()
+        eventDate = selectedDate
+        selectDateButton.text = selectedDate.toString()
+    }
+
+    fun showDatePicker(view: View) {
+        val fragment = DatePickerFragment(::onDateSelected)
+        fragment.show(supportFragmentManager, "datePicker")
+    }
+
+
 }
