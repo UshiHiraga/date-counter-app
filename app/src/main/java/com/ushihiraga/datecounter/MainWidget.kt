aa1caef59e1dd10c8282906a7a0502d4ee8931f7
Ushi Hiraga <ushi.hiraga@gmail.com>
2022-08-26 20:29:04 -0500

    Added classes for every activity.

---
+++ app/src/main/java/com/ushihiraga/datecounter/MainWidget.kt
@@ -0,0 +1,11 @@
+package com.ushihiraga.datecounter
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+
+class MainWidget : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, widgetsId: IntArray){
+
+    }
+}
